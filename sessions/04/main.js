feb6113f1eef11e6eac1b26dec919025b911f2b9
Glitch (yukseltron) <none>
2024-05-30 12:22:49 +0000

    ⛈👨‍❤️‍💋‍👨 Checkpoint ./sessions/04/style.css:60811391/938 ./sessions/04/index.html:60811391/297 ./sessions/04/main.js:60811391/409

--- sessions/04/main.js
+++ sessions/04/main.js
@@ -1,3 +1,15 @@
+function dreamEffect1() {
+  let element = document.getElementById("dream-01");
+  element.addEventListener("mouseover", () => {
+    document.body.style.backgroundColor = "green";
+  })
+  element.addEventListener("mouseout", () => {
+    document.body.style.backgroundColor = "black";
+  })
+}
+
+
+
 function snoozeEffect(element, zq) {
   const first = zq.shift();
   zq.push(first);
@@ -14,3 +26,4 @@ function startSnooze() {
 }
 
 startSnooze();
+dreamEffect1();
