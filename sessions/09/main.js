db582d42ba892bc429ab87070a57c1089e63a062
yukseltron <h.yuksel04@gmail.com>
2024-07-21 10:15:57 -0400

    09

--- sessions/09/main.js
+++ sessions/09/main.js
@@ -7,21 +7,6 @@ function alternateBreathing() {
     h1Element.textContent = states[currentStateIndex];
   }
 
-  function updateBalloon(num) {
-    const balloon = document.querySelector('.balloon');
-    balloon.style.width = `${balloon.style.width + num}px`;
-    balloon.style.height = `${balloon.style.height + num}px`;
-  }
-
-  function balloonState(currentState) {
-    if (currentState === 0) {
-      return 100;
-    } else if (currentState === 2) {
-      return -100;
-    } else {
-      return 0;
-    }
-  }
 
   function nextState() {
     currentStateIndex = (currentStateIndex + 1) % states.length;
@@ -29,8 +14,7 @@ function alternateBreathing() {
   }
 
   updateText();
-  updateBalloon(balloonState(currentStateIndex));
-  setInterval(nextState, 1000);
+  setInterval(nextState, 4000);
 }
 
 alternateBreathing();
