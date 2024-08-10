29fd5dd0fb4d910b4c8fc8b8a7be8d002a9524fb
yukseltron <h.yuksel04@gmail.com>
2024-08-10 09:58:08 -0400

    39

---
+++ sessions/39/main.js
@@ -0,0 +1,14 @@
+function toggleLamp1() {
+    var lamp = document.querySelector('.week-39');
+    lamp.style.backgroundColor = lamp.style.backgroundColor === 'orange' ? 'black' : 'orange';
+}
+
+function toggleLamp2() {
+    var lamp = document.querySelector('.week-39');
+    lamp.style.backgroundColor = lamp.style.backgroundColor === 'gold' ? 'black' : 'gold';
+}
+
+function toggleLamp3() {
+    var lamp = document.querySelector('.week-39');
+    lamp.style.backgroundColor = lamp.style.backgroundColor === 'wheat' ? 'black' : 'wheat';
+}
